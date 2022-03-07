d3b1da5974e59058272cf93ca7881d526e3b2f0f
mediboina9 <mediboyanasiva@gmail.com>
2022-03-07 22:34:50 +0530

    Synchrony interview question

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,26 @@
 package com.company;
 
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
        System.out.println("Hello Martina");
-        ArrayList<Integer> list=new ArrayList<Integer>(){
-            {
-             add(2);add(2);add(4);add(4);add(6);add(6);
-            }
-        };
+        FileReader file =new FileReader("C:\\Users\\Sony\\Downloads\\testing.txt");
+        BufferedReader fileInput=new BufferedReader(file);
+      //  BufferedWriter fileOutput = new BufferedWriter(file);
+      //  fileOutput.write("Siva write here");
+        ArrayList<String> list=new ArrayList<String>();
+        for(int i=0;i<10;i++){
+            //System.out.println(fileInput.readLine());
+            list.add(fileInput.readLine());
+
+        }
+        list.stream().distinct().forEach(System.out::println);
+
 
 
 
