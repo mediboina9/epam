6dade1c20aa2155c8eade594319168dcaa00e202
mediboina9 <mediboyanasiva@gmail.com>
2022-03-07 21:59:34 +0530

    IBM interview question input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,20 @@
 package com.company;
 
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        String input= "Hi My name is siva";
-
-        String[] array=input.split(" ");
-
-
-        for(int i=0;i< array.length;i++){
-            StringBuffer sr=new StringBuffer(array[i]);
-           // System.out.print(sr.reverse()+" ");
-
-
-        }
-        int n=5;
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=5;j++){
-                if(i>=j){
-                    System.out.print(n*(j-1)+i);
-                }
-                else{
-                    System.out.print(" ");
-                }
+       System.out.println("Hello Martina");
+        ArrayList<Integer> list=new ArrayList<Integer>(){
+            {
+             add(2);add(2);add(4);add(4);add(6);add(6);
             }
-            System.out.println();
+        };
 
-        }
+
 
 
 
