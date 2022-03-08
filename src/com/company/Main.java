2b821cd09e1aa667224697db41031834d4518f1e
mediboina9 <mediboyanasiva@gmail.com>
2022-03-08 19:35:01 +0530

    3question correct,static block and passing parameter new style( double... number)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,33 +7,60 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main {
-
+    static{
+        int j=10;
+        System.out.println("Hello"+10);
+    }
+    public void myMethod(){
+        System.out.println("Cool");
+    }
+    public Main(){
+        System.out.println("is a");
+    }
+    static {
+        System.out.println("Wallmart");
+    }
     public static void main(String[] args) throws IOException {
-
-       System.out.println("Hello Martina");
-        FileReader file =new FileReader("C:\\Users\\Sony\\Downloads\\testing.txt");
-        BufferedReader fileInput=new BufferedReader(file);
-      //  BufferedWriter fileOutput = new BufferedWriter(file);
-      //  fileOutput.write("Siva write here");
-        ArrayList<String> list=new ArrayList<String>();
-        for(int i=0;i<10;i++){
-            //System.out.println(fileInput.readLine());
-            list.add(fileInput.readLine());
+        Main obj=new Main();
+        obj.myMethod();
+       // System.out.println("1st question class hash code"+obj.hashCode());
+        //2ndQuestion
+        int[] arr={0,1,2,3,4,5,6};
+        int x=10,i=0,y;
+        try{
+            x=x/0;
+            i++;
+            y=10;
+            i++;
+        }
+        catch (ArithmeticException e){
+            i++;
+            System.out.println(i+"catch block");
+        }
+        finally {
+            i++;
+            System.out.println(i+"finally block");
 
         }
-        list.stream().distinct().forEach(System.out::println);
-        C.xyz();
+    System.out.println(arr[i]);
+        //3rd Question
+        hack_func2(44,4,4,1,485);
+        hack_func2(new double[]{1,2,3});
+        //4th Question
+        SubClass subClass=new SubClass(90);
+        System.out.println("4th Question"+subClass.number);
+    }
 
-        int arr[]={1,2,3,4,5,6,7,7};
-        ArrayList<Integer> list1=new ArrayList<>(){
-            {
-                add(1);add(2);add(3);add(2);add(4);add(2);
-            }
-        };
-
-        Collections.sort(list1);
-        System.out.println(list1.get(list1.size()-3));
-
+    private static void hack_func2(double... doubles) {
+    if(doubles.length==0){
+        System.out.println("No Array");
+    }
+    double result=doubles[0];
+    for(int i=1;i<doubles.length;i++)
+        if(doubles[i]>result) result=doubles[i];
+        System.out.println("highest Value"+result);
 
     }
+
+
 }
