b35e0d1adc95f3b96a9643f3c8eeac312e3e29fc
mediboina9 <mediboyanasiva@gmail.com>
2022-03-12 16:55:54 +0530

    count dupilicate word using hashMap, appending array of elements into single string variable

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,7 +15,6 @@ public class Main {
         for(int i=0;i< array.length;i++){
             StringBuffer sr=new StringBuffer(array[i]);
            // System.out.print(sr.reverse()+" ");
-
 
         }
         int n=5;
@@ -29,6 +30,46 @@ public class Main {
             System.out.println();
 
         }
+        //Count number of repeated words
+        String sentance="Hi Krisha , How Are you . Hi Siva , I am fine";
+        HashMap<String, Integer> hashMap=new HashMap<>();
+        String[] arr1=sentance.split(" ");
+
+        for(int i=0;i< arr1.length;i++){
+             if(hashMap.containsKey(arr1[i])==false){
+                hashMap.put(arr1[i],1);
+            }
+           else if(hashMap.containsKey(arr1[i])==true ){
+              //  int count=hashMap.get(arr1[i])+1;
+                hashMap.put(arr1[i],hashMap.get(arr1[i])+1);
+            }
+
+        }
+       /* for(int i=0;i< arr1.length;i++){
+          //  System.out.println(arr1[i]+" value");
+
+             System.out.println(hashMap.get(i)+"hashMap value");
+        }*/
+       // for(Integer i:hashMap.values()) System.out.println(i + "hashMap value");
+       String key= hashMap.entrySet().stream().max((e1,e2)->e1.getValue() > e2.getValue()?1:1). get().getKey();
+       System.out.print("Higest repeated key is:");
+       System.out.println(key+" and Count is:"+hashMap.get(key));
+
+        String hasCode1="abc";
+        String hasCode2=new String("abc");
+        String hasCode3="abc";
+        System.out.println(hasCode1.hashCode()==hasCode3.hashCode());
+        System.out.println(hasCode1.hashCode()==hasCode2.hashCode());
+
+        //String appending without useing buffer/bullder
+        String si="";
+      for(int i=0;i< arr1.length;i++){
+          si+=arr1[i];
+
+
+        }
+      System.out.println(si);
+
 
 
 
