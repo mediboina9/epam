f7b4bef4019ed2d910d51080c0960568d50a613a
mediboina9 <mediboyanasiva@gmail.com>
2022-03-15 10:22:08 +0530

    altimetric 3rd round

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -1,42 +1,13 @@
 package com.company;
 
 public class Employee {
-    private  int id;
+    private int id;
     private String name;
+    private int dept_id;
 
-    public Employee(int id, String name) {
+    public Employee(int id, String name, int dept_id) {
         this.id = id;
         this.name = name;
-    }
-    public static int getStaticMethod(){
-        System.out.println("Static void method");
-        return 5;
-    }
-    public void getNonStaticMethod(){
-        System.out.println(getStaticMethod());
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        this.dept_id = dept_id;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,76 +1,44 @@
 package com.company;
 
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String input= "Hi My name is siva";
+       int arr[]={1,5,3,8,5};
+       int j,temp;
+       for(int i=2;i<arr.length;i++) {
+           j = i - 1;
+           temp = arr[i];
 
-        String[] array=input.split(" ");
+           while (j >= 0 && arr[j] > temp) {
+               arr[j + 1] = arr[j];
+               j--;
+           }
+           arr[j + 1] = temp;
+       }
+        Arrays.stream(arr).forEach(System.out::println);
 
 
-        for(int i=0;i< array.length;i++){
-            StringBuffer sr=new StringBuffer(array[i]);
-           // System.out.print(sr.reverse()+" ");
+/*
+oddList=list.stream().filter(e->e.getId()%2==0).collect(Collecotrs.toList);
 
+HashMap<Integer, Integer> hashMap=new HashMap<>();
+    if(int i=0;i<oddList.size();i++){
+        if(hashMap.get(oddList.get(i))==null)
+        {
+            hashMap.put(oddList.get(i),1);
         }
-        int n=5;
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=5;j++){
-                if(i>=j){
-                    System.out.print(n*(j-1)+i);
-                }
-                else{
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-
+        else if(hashMap.get(oddList.get(i))>0)
+        {
+            hashMap.put(oddList.get(i),hashMap.get(oddList.get(i))+1);
         }
-        //Count number of repeated words
-        String sentance="Hi Krisha , How Are you . Hi Siva , I am fine";
-        HashMap<String, Integer> hashMap=new HashMap<>();
-        String[] arr1=sentance.split(" ");
-
-        for(int i=0;i< arr1.length;i++){
-             if(hashMap.containsKey(arr1[i])==false){
-                hashMap.put(arr1[i],1);
-            }
-           else if(hashMap.containsKey(arr1[i])==true ){
-              //  int count=hashMap.get(arr1[i])+1;
-                hashMap.put(arr1[i],hashMap.get(arr1[i])+1);
-            }
-
         }
-       /* for(int i=0;i< arr1.length;i++){
-          //  System.out.println(arr1[i]+" value");
 
-             System.out.println(hashMap.get(i)+"hashMap value");
-        }*/
-       // for(Integer i:hashMap.values()) System.out.println(i + "hashMap value");
-       String key= hashMap.entrySet().stream().max((e1,e2)->e1.getValue() > e2.getValue()?1:1). get().getKey();
-       System.out.print("Higest repeated key is:");
-       System.out.println(key+" and Count is:"+hashMap.get(key));
-
-        String hasCode1="abc";
-        String hasCode2=new String("abc");
-        String hasCode3="abc";
-        System.out.println(hasCode1.hashCode()==hasCode3.hashCode());
-        System.out.println(hasCode1.hashCode()==hasCode2.hashCode());
-
-        //String appending without useing buffer/bullder
-        String si="";
-      for(int i=0;i< arr1.length;i++){
-          si+=arr1[i];
-
-
-        }
-      System.out.println(si);
-
-
+    hashMap.entrySet().stream().forEach(System.out::println);*/
 
 
     }
