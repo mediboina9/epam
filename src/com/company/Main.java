9c48e2f24ece112a162cb095d92eb09761876a9c
mediboina9 <mediboyanasiva@gmail.com>
2022-03-24 20:26:19 +0530

    fail-safe CopyOnWriteArrayList

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
 
@@ -18,10 +19,33 @@ public class Main {
         itr=al.iterator();
         while (itr.hasNext()){
             if(itr.next()==4){
-                al.remove(2);
+                itr.remove();
             }
         }
         System.out.println(al);
+        CopyOnWriteArrayList<Integer> list=new CopyOnWriteArrayList<>(new Integer[]{1,2,3,4});
+        Iterator<Integer> itrList=list.listIterator();
+
+        while (itrList.hasNext()){
+            Integer no=(Integer) itrList.next();
+            System.out.println(no);
+            //every time 14 will be added into list
+            if(true){
+                list.add(14);
+            }
+            //for particular location we need to condition in if loop
+            if(no==4){
+                list.add(15);
+            }
+        }
+       itrList=list.listIterator();
+
+        while (itrList.hasNext()){
+            Integer no=(Integer) itrList.next();
+            System.out.println(no);
+
+        }
+        //CuncurrentHashMap
 
 
 
