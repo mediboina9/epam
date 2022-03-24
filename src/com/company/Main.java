2fcb043f2d1b1d7c8961e48df6b9466e1fad19a5
mediboina9 <mediboyanasiva@gmail.com>
2022-03-24 20:09:54 +0530

    fail-fast Cuncurrent modification exception

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,29 @@
 package com.company;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+    ArrayList<Integer> al=new ArrayList<>();
+    al.add(1);al.add(2);al.add(3);al.add(4);
+        Iterator<Integer> itr=al.iterator();
+        while (itr.hasNext()){
+            if(itr.next()==3){
+                itr.remove();
+            }
+        }
+        System.out.println(al);
+        itr=al.iterator();
+        while (itr.hasNext()){
+            if(itr.next()==4){
+                al.remove(2);
+            }
+        }
+        System.out.println(al);
 
-    ImmutableClass obj=new ImmutableClass(1,"Siva");
-        System.out.println(obj);
 
-        obj=new ImmutableClass(2,"Siva");
-        System.out.println(obj);
-
-        String s1="siva";
-        String s2="Satya";
-        System.out.println(s1+" hashcode "+s1.hashCode());
-        s1=s1+"satya";
-        System.out.println(s1+" hashcode "+s1.hashCode());
-        s1="siva";
-        System.out.println(s1+" hascode "+s1.hashCode());
-
-        System.out.println(s2+" hashcode "+s2.hashCode());
-        String s3=s1+s2;
-        System.out.println(s3+" hashcode "+s3.hashCode());
-        int a=10;
-        int b=20;
-        System.out.println(1+2+a+b);
 
     }
 
